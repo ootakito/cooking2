02154e40ef15d34f73d96ace34a399301db8b6be
ootakito <165157559+ootakito@users.noreply.github.com>
2024-05-06 13:13:38 +0900

    復旧作業

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :encrypted_password, presence: true
+  #has_many :recipes
+  #has_many :likes
+end
