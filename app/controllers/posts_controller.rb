d8d597e62adafe2c3abcc8fdc7fad4f473588dc6
ootakito <165157559+ootakito@users.noreply.github.com>
2024-05-07 17:29:41 +0900

    indexプロトタイプがある程度完成した

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
   def index
-    @posts = Post.all
+    @posts = Post.order(created_at: :desc).limit(6)
   end
 
   def new
