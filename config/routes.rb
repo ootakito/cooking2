59a2dd93dd24b0ba88b88690a04ecceb083a3a4f
ootakito <165157559+ootakito@users.noreply.github.com>
2024-05-14 15:41:51 +0900

    ランサック導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
+
   resources :posts do
     resource :likes, only: [:create, :destroy]
+    collection do
+      get 'search'
+    end
   end
+
   root "posts#index"
   get 'mypage/index'
   get 'popularposts/index'
