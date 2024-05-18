8ffa5eafa2513fe251b5219e56da4b1b8b7f3959
ootakito <165157559+ootakito@users.noreply.github.com>
2024-05-18 11:44:41 +0900

    postテーブルのlikeに追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
-  has_many :likes
+  has_many :likes, dependent: :destroy
   has_one_attached :image
   validates :title, presence: true
   validates :description, presence: true, length: { maximum: 200 }
