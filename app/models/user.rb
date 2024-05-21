b7735ac08b8cbf77eb53edbe371d328eb1e88d30
ootakito <165157559+ootakito@users.noreply.github.com>
2024-05-21 23:13:16 +0900

    ユーザーバリデーションを追加した。

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  validates :last_name, presence: true
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
   validates :first_name, presence: true
   validates :email, presence: true, uniqueness: true
-  validates :encrypted_password, presence: true
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
   has_many :posts
   has_many :likes
 end
